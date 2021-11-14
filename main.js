7274fe9c4f038679cf218ab386d778877035af1c
yamatomiyoshi <92811611+yamatomiyoshi@users.noreply.github.com>
2021-11-14 14:58:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ let char2 = { x: 790, y: 790, isWarpAllows: true };
 var music = new Audio();
 function bgm() {
     music.preload = "auto";
-    music.src = "../ネバーロンチBGM.m4a";
+    music.src = "./ネバーロンチBGM.m4a";
     music.load();
 
     music.addEventListener("ended", function () {
